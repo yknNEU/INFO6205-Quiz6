2ac4159fb3ba22e33d76f5e7b7d55d68df6a2378
yknNEU <181112251+yknNEU@users.noreply.github.com>
2025-03-27 13:09:30 -0400

    Finish

--- src/main/java/QuickSort.java
+++ src/main/java/QuickSort.java
@@ -20,13 +20,29 @@ public class QuickSort {
     // quicksort the subarray from a[lo] to a[hi]
     private static void sort(Comparable[] a, int lo, int hi) {
         if (hi <= lo) return;
-       //Student TODO
+        int j = partition(a, lo, hi);
+        sort(a, lo, j-1);
+        sort(a, j+1, hi);
     }
 
     // partition the subarray a[lo..hi] so that a[lo..j-1] <= a[j] <= a[j+1..hi]
     // and return the index j.
     private static int partition(Comparable[] a, int lo, int hi) {
-        //Student TODO
+        int i = lo, j = hi + 1;
+        Comparable v = a[lo];
+
+        while (true) {
+            while (less(a[++i], v)) {
+                if (i == hi) break;
+            }
+            while (less(v, a[--j])) {
+                if (j == lo) break;
+            }
+            if (i >= j) break;
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+        return j;
     }
 
     
